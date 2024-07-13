e1c75b01437e0a3db704a880e689cab2e028973d
culorin <culorin@users.noreply.github.com>
2024-07-13 12:52:59 +0200

    feat: menu button styling, credits itr1

--- Assets/Modules/UI/Runtime/MainMenu/MainMenu.cs
+++ Assets/Modules/UI/Runtime/MainMenu/MainMenu.cs
@@ -6,16 +6,30 @@ public class MainMenu : MonoBehaviour
 {
     public string startScene = "SC_L01_KidsRoom";
 
+    private Button buttonStartGame;
+    private Button buttonShowOptions;
+    private Button buttonShowCredits;
+    private Button buttonQuit;
+
+    private VisualElement credits;
+
     private void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-        Button buttonStartGame      = root.Q<Button>("ButtonStartGame");
-        Button buttonShowOptions    = root.Q<Button>("ButtonShowOptions");
-        Button buttonShowCredits    = root.Q<Button>("ButtonShowCredits");
-        Button buttonQuit           = root.Q<Button>("ButtonQuit");
+        buttonStartGame   = root.Q<Button>("ButtonStartGame");
+        buttonShowOptions = root.Q<Button>("ButtonShowOptions");
+        buttonShowCredits = root.Q<Button>("ButtonShowCredits");
+        buttonQuit        = root.Q<Button>("ButtonQuit");
+
+        credits = root.Q<VisualElement>("Credits");
 
         buttonStartGame.clicked += () => SceneManager.LoadScene(startScene);
+        buttonShowCredits.clicked += () => credits.visible = !credits.visible;
         buttonQuit.clicked += () => Application.Quit();
+
+        buttonStartGame.Focus();
+
+        Debug.Assert(buttonStartGame == GetComponent<UIDocument>().rootVisualElement.focusController.focusedElement);
     }
 }
